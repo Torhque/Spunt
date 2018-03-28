217a3cdab58c883b4a724e54eab0c4ea7e60c03c
Torhque <Torhque@live.com>
2018-03-27 18:44:40 -0700

    Add files via upload

---
+++ Utility/Tag.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tag : MonoBehaviour
+{
+	public static Tag instance;
+
+	public bool isWaypoint; // Tag for waypoint objects
+	public bool isSpawnPoint;
+	public bool isPlayer; // Tag for player objects
+}
